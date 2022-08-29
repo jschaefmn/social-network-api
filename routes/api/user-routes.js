bd516ad3e41f82053dbb57cdaf6c7513864bd21d
Jake Schaefbauer <jschaefmn@gmail.com>
2022-08-29 09:20:43 -0500

    created user routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,32 @@
+const Router = require('express').Router();
+
+const {
+  getUsers,
+  getUserById,
+  addUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend
+} = require('../../controllers/user-controller');
+
+// Routes for users
+router 
+  .route('/')
+  .get(getUsers)
+  .post(addUser);
+
+// Routes for getting user by Id
+router
+  .route('/:id')
+  .get(getUserById)
+  .post(updateUser)
+  .delete(deleteUser);
+
+// Routes for getting friends by Id
+router
+.route('/:id/friends/:friendId')
+.put(addFriend)
+.delete(deleteFriend);
+
+module.exports = router
